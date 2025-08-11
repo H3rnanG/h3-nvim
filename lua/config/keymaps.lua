08cbd161bd3c08ad361641b2f05be77a4e7d3d6c
h3rnan <h3rnangamarr@gmail.com>
2025-08-11 16:24:38 -0500

    first commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,4 @@
+vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = 'Quit' })
+vim.keymap.set('n', '<leader>Q', ':quita<CR>', { desc = 'Quit All' })
+
+vim.keymap.set('n', '<leader>l', ':Lazy<CR>', { desc = 'Open Lazy' })

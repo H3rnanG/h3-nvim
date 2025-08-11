7b1674acb6025261092da784a6ec21179848c67f
h3rnan <h3rnangamarr@gmail.com>
2025-08-11 18:25:55 -0500

    feat: update key mappings for quitting and Fzf functionality

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,6 @@
-vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = 'Quit' })
-vim.keymap.set('n', '<leader>Q', ':quita<CR>', { desc = 'Quit All' })
+vim.keymap.set('n', '<leader>qq', ':q<CR>', { desc = 'Quit' })
+vim.keymap.set('n', '<leader>qa', ':quita<CR>', { desc = 'Quit All' })
+vim.keymap.set('n', '<leader>qb', ':bd<CR>', { desc = 'Quit Buffer' })
 
 vim.keymap.set('n', '<leader>l', ':Lazy<CR>', { desc = 'Open Lazy' })
 vim.keymap.set('n', '<leader>m', ':Mason<CR>', { desc = 'Open Mason' })
@@ -8,3 +9,4 @@ vim.keymap.set('n', '<leader>m', ':Mason<CR>', { desc = 'Open Mason' })
 vim.keymap.set('n', '<leader>ff', ':FzfLua files<CR>', { desc = 'Find Files' })
 vim.keymap.set('n', '<leader>fg', ':FzfLua live_grep<CR>', { desc = 'Live Grep' })
 vim.keymap.set('n', '<leader>fb', ':FzfLua buffers<CR>', { desc = 'Buffers' })
+vim.keymap.set('n', '<leader>fh', ':FzfLua helptags<CR>', { desc = 'Help Tags' })

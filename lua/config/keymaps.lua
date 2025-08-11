ce331653469a172a3b67e7488f3b1cd23d9abe00
h3rnan <h3rnangamarr@gmail.com>
2025-08-11 16:40:53 -0500

    feat: add fzf-lua plugin configuration and key mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,8 @@ vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = 'Quit' })
 vim.keymap.set('n', '<leader>Q', ':quita<CR>', { desc = 'Quit All' })
 
 vim.keymap.set('n', '<leader>l', ':Lazy<CR>', { desc = 'Open Lazy' })
+
+-- Fzf
+vim.keymap.set('n', '<leader>ff', ':FzfLua files<CR>', { desc = 'Find Files' })
+vim.keymap.set('n', '<leader>fg', ':FzfLua live_grep<CR>', { desc = 'Live Grep' })
+vim.keymap.set('n', '<leader>fb', ':FzfLua buffers<CR>', { desc = 'Buffers' })

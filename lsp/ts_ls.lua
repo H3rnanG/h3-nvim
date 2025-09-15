f2b83d7a14e50c1914d693446cc219eae2e4a710
h3rnan <h3rnangamarr@gmail.com>
2025-09-15 12:18:36 -0500

    feat: html, css, ts and astro lsp

---
+++ lsp/ts_ls.lua
@@ -0,0 +1,25 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+return {
+  name = 'ts_ls',
+  cmd = { 'typescript-language-server', '--stdio' },
+  filetypes = {
+    'javascript',
+    'javascriptreact',
+    'typescript',
+    'typescriptreact',
+  },
+  root_dir = vim.fs.dirname(vim.fs.find({
+    'tsconfig.json',
+    'package.json',
+    '.git'
+  }, { upward = true })[1]),
+  init_options = {
+    hostInfo = 'neovim',
+    typescript = {
+      tsdk = '/usr/lib/node_modules/typescript/lib',
+    },
+  },
+  capabilities = capabilities,
+}

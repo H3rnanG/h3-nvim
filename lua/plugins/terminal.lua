d1d6e941c1f77d210090a8a87affbbf6a6a0a281
h3rnan <h3rnangamarr@gmail.com>
2025-09-17 00:00:08 -0500

    feat: add animations and terminal plugin configurations

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,14 @@
+return {
+  "luxvim/nvim-luxterm",
+  config = function()
+    require("luxterm").setup({
+      manager_width = 0.8,
+      manager_height = 0.8,
+      preview_enabled = true,
+      auto_hide = true,
+      keymaps = {
+        toggle_manager = "<C-/>",
+      }
+    })
+  end
+}
